301473b594bdce4d757328117f3024b8eb927609
mbote <mbotepm@gmail.com>
2022-06-09 15:48:55 +0100

    update server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
-import expess from "express"
+//import "express-async-errors";
+import expess, {Request, Response, NextFunction} from "express";
+import { router } from "./routes"
 
-const app = expess()
+const app = expess();
+
+app.use(expess.json());
+
+app.use(router);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    return response.json({
+        status: "Error",
+        message: error.message
+    })
+})
 
 app.listen(3000, () => console.log('server is running on port 3000'))
 
