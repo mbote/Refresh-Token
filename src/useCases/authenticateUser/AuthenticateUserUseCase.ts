94bddf936d21bff410cac5d84eb9b90c1d096058
mbote <mbotepm@gmail.com>
2022-06-09 15:49:41 +0100

    create user authenticate

---
+++ src/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,38 @@
+
+//import {compare} from "bcryptjs"
+import {sign} from "jsonwebtoken"
+import {client} from "../../prisma/client"
+
+interface IRequest{
+    username: string,
+    password: string
+}
+
+class AuthenticateUserUseCase{
+
+    async execute({username, password}:IRequest ){
+        const userAlreadyExists = await client.user.findFirst({
+            where:{
+                username,
+            }
+        });
+        if(!userAlreadyExists){
+            throw new  Error("User or password incorrect!");
+        }
+       /*  const passwordMatch = await compare(password, userAlreadyExists.password)
+
+        if(!passwordMatch){
+            throw new  Error("User or password incorrect!");
+        }*/
+    
+       const token = sign({}, "uiugeneration",{
+           subject: userAlreadyExists.id,
+           expiresIn:"20s"
+       })
+
+       return { token };
+    }
+
+}
+
+export{AuthenticateUserUseCase}
